97a383da0f668c547ccd8be18f9d3e274b470cba
mamonde456 <mamonde456@gmail..com>
2022-02-20 19:22:42 +0900

    index, weater 수정

--- js/weather.js
+++ js/weather.js
@@ -7,10 +7,12 @@ function onGe0k(position) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector(".weather span:first-child");
-      const city = document.querySelector(".weather span:last-child");
+      const weather = document.querySelector(".weather-inner");
+      const temperature = document.querySelector(".temperature-inner");
+      const city = document.querySelector(".place");
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} /`;
+      temperature.innerText = ` ${data.main.temp}`;
     });
 }
 
